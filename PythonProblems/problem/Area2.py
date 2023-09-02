d943f139ff76954b97d73c6fd296c95898fbd1c0
Sarweshwaran <sarweshchandran@gmail.com>
2023-09-02 13:27:48 +0530

    Added the Calculation of Perimeter to the Rectangle

--- PythonProblems/problem/Area2.py
+++ PythonProblems/problem/Area2.py
@@ -12,12 +12,17 @@ class Rectangle:
         Cube = self.length * self.width * self.height
         return Cube
 
+    def perimeter(self):
+        P = 2(self.length+ self.width)
+        return P
+
     def function(self):
         Area = self.calculateArea()
         Cube = self.calculateCube()
+        P = self.perimeter()
         print("Area of rectangle with length" , self.length ," cm" ," and width " , self.width ,"cm is:" , Area)
         print("Volume of rectangle with length", self.length, " cm", "width ", self.width ," cm and height " ,self.height, " cm is: ", Cube)
-
+        print("Perimeter of Rectangle is :" ,P)
 if __name__ == "__main__":
     length = int(input("Enter the length of the rectangle: "))
     width = int(input("Enter the width of the rectangle: "))
